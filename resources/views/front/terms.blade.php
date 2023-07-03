9c9c472de4f3c6a8db3b03af55dc1e9669d8f1e2
shriradharani <brij.phpdev@gmail.com>
2023-07-03 22:36:39 +0530

    Terms page update

---
+++ resources/views/front/terms.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.front.theme')
+
+
+@section("content")
+
+<div class="container-fluid bg-primary hero-header">
+    <div class="container ">
+        <div class="row justify-content-center ">
+            <div class="col-lg-10 pt-lg-5 mt-lg-5 text-center">
+                <h1 class="display-3 text-white animated slideInDown">Terms of use</h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb justify-content-center">
+                        <li class="breadcrumb-item"><a class="text-dark" href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a class="text-dark" href="#">Pages</a></li>
+                        <li class="breadcrumb-item text-white active" aria-current="page">Terms of use</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Contact Start -->
+<div class="container-xxl py-5 mt-5">
+    <div class="container">
+        <div class="text-center">
+            <h6 class="section-title bg-white text-center text-primary px-3">Terms of use</h6>
+            <h1 class="">Guidelines for Landlords or Reviewers</h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-12 col-md-12">
+                <!--<h5> </h5>-->
+                <p class="mb-4"><a class="text-danger">Dodgyone.com</a> believes that everyone has the power to shape and improve the world. We do this by providing a powerful, open-to-all review platform where landlords or reviewers can share their experiences with doggy tenants, and help other landlords before they re-let their property to dodgy ones. 
+                </p>
+                
+
+            </div>
+
+            <div class="col-lg-12 col-md-12">
+                <h5>When to write a review</h5>
+                <p class="mb-4">This is because we want to ensure that all reviews on dodgyone.com are genuine and unbiased. We believe that the best way to get accurate information about a business is to hear from people who have had real experiences with it. If people are incentivized to write positive reviews, it can distort the overall picture and make it difficult for people to make informed decisions.</p> 
+            </div>
+
+            <div class="col-lg-12 col-md-12">
+                <h5>Keep private information private</h5>
+                <p class="mb-4">Your reviews are public, so it is important to be mindful of the information you include. Do not share your own sensitive or personal information, such as your name, phone number, address, email address, or anything else that could be used to track, identify, contact, or impersonate you. It is also not okay to share someone else's sensitive or personal information.</p> 
+                <p class="mb-4"> We want to protect the privacy and safety of everyone who uses our platform. By following these guidelines, you can help us ensure that everyone has a positive experience.</p> 
+                <div class="d-flex align-items-center mb-4">
+                    <div class="d-flex align-items-center justify-content-center flex-shrink-0">
+                        <p>
+
+                            Here are some additional examples of sensitive information that you should not include in your reviews:
+
+                        </p>
+                        <ul>
+                            <li>Social security numbers</li>
+                            <li>Credit card numbers</li>
+                            <li>Medical information</li>
+                            <li>Religious or political beliefs</li>
+                            <li>Sexual orientation</li>
+                            <li>Any other information that you would not want to be publicly known</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-md-12">
+                <h5>The final say</h5>
+                <p class="mb-4">Thank you for your understanding. We appreciate your cooperation in helping us to maintain the integrity of our platform.</p> 
+                <div class="d-flex align-items-center mb-4">
+                    <div class="d-flex align-items-center justify-content-center flex-shrink-0">
+                        <p>Here are some additional things to keep in mind:</p>
+                        <ul>
+                            <li>These guidelines are subject to change at any time.</li>
+                            <li>We reserve the right to remove any review that we deem to be in violation of these guidelines.</li>
+                            <li>If you have any questions about these guidelines, please contact us.</li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="mb-4">Thank you for your cooperation.</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- Contact End -->
+
+
+@endsection
