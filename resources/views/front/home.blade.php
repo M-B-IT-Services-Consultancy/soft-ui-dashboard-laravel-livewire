b6049e2df10c3cbb555dfe88f09c628fcef274a6
shriradharani <brij.phpdev@gmail.com>
2023-07-06 16:23:35 +0530

    update home search

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -10,7 +10,7 @@
                         <h1 class="display-5 text-white mb-3 animated slideInDown">Read reviews. Write Reviews.</h1>
                         <p class="fs-4 text-white mb-4 animated slideInDown">Search for <a class="text-warning">reviews</a> provided by other landlord about your new <a class="text-danger">tenant</a></p>
                         <div class="position-relative w-75 mx-auto animated slideInDown">
-                            <input class="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Eg: Tenant name, Passport #, DL #">
+                            <input class="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Eg: Tenant name, Property, City, Passport #, DL #">
                             <button type="button" class="btn btn-primary rounded-pill py-2 px-4 position-absolute top-0 end-0 me-2" style="margin-top: 7px;">Search</button>
                         </div>
                     </div>
